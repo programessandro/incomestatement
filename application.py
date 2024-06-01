b925184d6b6e4d0cd8be566ac25ee98bdd708e43
programessandro <153205915+programessandro@users.noreply.github.com>
2024-06-01 18:18:08 +0200

    Update application configuration

--- application.py
+++ application.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify
-import pandas as pd
 from sec_api import XbrlApi
-from sec_edgar_downloader import Downloader
-from sec_downloader.types import RequestedFilings
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,35 +8,38 @@ API_KEY = "9cdd3722b1fe0ca3e4249d93711530f82771e62cf679e8dff1a5953acf8df5cf"
 xbrlApi = XbrlApi(API_KEY)
 
 @app.route('/income_statement', methods=['GET'])
-def get_income_statement():
+def income_statement():
     ticker = request.args.get('ticker')
     if not ticker:
         return jsonify({"error": "Ticker is required"}), 400
 
-    dl = Downloader("anything", "dellacm@usi.ch")
-    metadatas = dl.get_filing_metadatas(RequestedFilings(ticker_or_cik=ticker, form_type="10-K", limit=1))
+    try:
+        # Replace 'Ticker' with the actual ticker value
+        metadatas = dl.get_filing_metadatas(
+            RequestedFilings(ticker_or_cik=ticker, form_type="10-K", limit=1)
+        )
+        if metadatas:
+            primary_doc_url = metadatas[0].primary_doc_url
+            xbrl_json = xbrlApi.xbrl_to_json(htm_url=primary_doc_url)
+            income_statement = get_income_statement(xbrl_json)
+            return income_statement.to_json()
+        else:
+            return jsonify({"error": "No filings found for ticker"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-    if not metadatas:
-        return jsonify({"error": "No filings found for ticker"}), 404
-
-    primary_doc_url = metadatas[0].primary_doc_url
-    xbrl_json = xbrlApi.xbrl_to_json(htm_url=primary_doc_url)
-
-    income_statement = process_income_statement(xbrl_json)
-    return income_statement.to_json()
-
-def process_income_statement(xbrl_json):
+def get_income_statement(xbrl_json):
     income_statement_store = {}
     for usGaapItem in xbrl_json['StatementsOfIncome']:
         values = []
-        indices = []
+        indicies = []
         for fact in xbrl_json['StatementsOfIncome'][usGaapItem]:
             if 'segment' not in fact:
                 index = fact['period']['startDate'] + '-' + fact['period']['endDate']
-                if index not in indices:
+                if index not in indicies:
                     values.append(fact['value'])
-                    indices.append(index)
-        income_statement_store[usGaapItem] = pd.Series(values, index=indices)
+                    indicies.append(index)
+        income_statement_store[usGaapItem] = pd.Series(values, index=indicies)
     income_statement = pd.DataFrame(income_statement_store)
     return income_statement.T
 
